709ec8faa904d2692b2cf542552501b21272d15d
Rodrigo Motta <rodmottaads@gmail.com>
2021-11-26 11:48:48 -0300

    pequena mudança na configuração dos Cors

--- src/main/java/com/github/rodmotta/customerbackend/config/CorsConfig.java
+++ src/main/java/com/github/rodmotta/customerbackend/config/CorsConfig.java
@@ -9,8 +9,6 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+        registry.addMapping("/**").allowedMethods("*");
     }
 }
